602d1cf64acb6dd41a85a1e241e0986de0a47c1e
Monte-Cristo78 <vchurakov55@gmail.com>
2022-10-07 09:14:37 +0300

    Completed 1/5

--- hw_01_4/cpp/main.cpp
+++ hw_01_4/cpp/main.cpp
@@ -2,9 +2,21 @@
 #include <string>
 
 int main() {
-	std::string str = "Hello, World";
+	std::string name = "Vlad";
+	std::string surname = "Churakov";
+	std::string patronymic = "Ivanovich";
 
-	std::cout << str;
+	std::string phoneNumber = "+380 000 000 00 01";
+	std::string email = "vadim@gmail.com";
+
+	std::string productName = "football club";
+	int productPrice = 1670000000;
+	int productAmount = 1;
+
+
+	std::cout << name << ' ' << surname << ' ' << patronymic << std::endl;
+	std::cout << phoneNumber << std::endl << email << std::endl;
+	std::cout << productName << std::endl << '$' << productPrice << std::endl << productAmount;
 
 	return 0;
 }
